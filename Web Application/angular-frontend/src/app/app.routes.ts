3c76aeaf60a27ae06afcb9cf126568b7f4ca2ceb
CriBerna1703 <criberna1703@gmail.com>
2025-03-15 09:41:16 +0100

    Final fix docker-compose and dummy routes

--- Web Application/angular-frontend/src/app/app.routes.ts
+++ Web Application/angular-frontend/src/app/app.routes.ts
@@ -1,11 +1,5 @@
 import { Routes } from '@angular/router';
-import { FilterMaskComponent } from './filter-mask/filter-mask.component';
-import { GraphViewComponent } from './graph-view/graph-view.component';
-import { TemporalViewComponent } from './temporal-view/temporal-view.component';
 
 export const routes: Routes = [
-  { path: 'filter', component: FilterMaskComponent },
-  { path: 'graph', component: GraphViewComponent },
-  { path: 'temporal', component: TemporalViewComponent },
-  { path: '', redirectTo: '/graph', pathMatch: 'full' }
+  { path: '', pathMatch: 'full' }
 ];
